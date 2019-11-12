19647110fe6f59e097885f51c837ea4835b01d42
Akshayrp <akshaypatwari24@gmail.com>
2019-11-12 12:21:02 +0530

    flipCoin with looping

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,14 +1,32 @@
-#! /bin/bash
+#!/bin/bash -x
 
-
+declare -A flipcoinresults
+counter=0;
 echo "                                  "
 echo "------------Greetings-------------"
+headCount=0;
+read -p "How many times you want to flip the coin " loopingtime
 
-randomCheck=$(( RANDOM % 2 ))
+function flipCoin() {
+	for(( count=1; count <= $loopingtime; count++ ))
+	do
+	randomCheck=$(( RANDOM % 2 ))
 
 	if [ $randomCheck -eq 1 ]
 	then
+		flipcoinresults[$count]=$randomCheck
+		headCount=$(( $headCount + 1 ))
 		echo "It's Head"
 	else
 		echo "It's Tail"
+		flipcoinresults[$count]=$randomCheck
 	fi
+	done
+}
+
+flipCoin
+echo "-----" $headCount
+headPercent=$(( $headCount * 100 / $loopingtime ))
+echo ${flipcoinresults[@]}
+echo "percentage of getting head is" $headPercent
+
