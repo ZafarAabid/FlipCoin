8d1790135bea8cd57b41e4e59c4b8a0f6aec2746
Akshayrp <akshaypatwari24@gmail.com>
2019-11-12 11:20:21 +0530

    initial Flip coin program

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,3 +1,14 @@
 #! /bin/bash
 
+
+echo "                                  "
 echo "------------Greetings-------------"
+
+randomCheck=$(( RANDOM % 2 ))
+
+	if [ $randomCheck -eq 1 ]
+	then
+		echo "It's Head"
+	else
+		echo "It's Tail"
+	fi
